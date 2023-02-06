04a3865fd994e16c32836f39b8e502df433158d7
ReginaPechenuk <paridjanka@mail.ru>
2023-02-06 07:26:11 +0300

    task 34

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] FillArray(int length, int start, int end)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+int getUserData(string massage)
+{
+    Console.WriteLine(massage);
+    int UserData = int.Parse(Console.ReadLine()!);
+    return UserData;
+}
+
+int FindCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2 == 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int number = getUserData("Введите длинну массива");
+int [] arr = FillArray(number,0,999);
+PrintArray(arr);
+int count1 = FindCount(arr);
+Console.WriteLine($"Количество четных чисел в массиве = {count1}");
